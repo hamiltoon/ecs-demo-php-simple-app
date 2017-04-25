469f204a3af03a2505ed87afd0f6c0543d965444
Hamiltoon <john@publit.se>
2017-04-25 11:38:56 +0200

    Testing CI

--- src/index.php
+++ src/index.php
@@ -15,9 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <h1>John är bäst!</h1>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
